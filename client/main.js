2cfd23bf8dc173b5f265b0c13b5c84da08e8a7d5
jacob <jacob@jacob.local>
2016-01-14 14:20:47 -0500

    initial commit

---
+++ client/main.js
@@ -0,0 +1,117 @@
+var monthDifference;
+var yearDifference;
+var yearsAndMonths;
+var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+$(document).ready(function(){
+
+	$('#start-date').val('2014-08-23').focus();
+	// $('#calendar').datepicker({
+	// 	inline: true,
+	// 	changeMonth: false,
+	// 	firstDay: 1,
+	// 	showOtherMonths: false,
+	// 	dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] 
+	// });	
+	$( "#date-picker" ).datepicker({
+		changeYear: true
+	});
+
+	$('#start-date').on('blur',function(){
+        if($(this).val()){
+			$('.timelineVertical ul').empty();	        	
+	        var date = new Date($(this).val());
+	        var today = new Date();
+	        yearDifference = calculateYears(date);
+	        monthDifference = monthDiff(date,today);			
+	        yearsAndMonths = monthDifference - (yearDifference * 12);
+	        var startYear = date.getFullYear();
+	        for(var i=0;i<yearDifference;i++){
+		        $('.bubbleUlVert').append('<li></li>');
+		        $('.verticalLineUlMobile').append('<li class="verticalLineMobile"></li>');
+		        $('.dateDivVert').append('<li class="specificDateVert" style="color: rgb(245, 130, 158);">'+startYear+'</li>');
+		        startYear++;
+	        }
+	        var currentMonth=today.getMonth();
+	        var dateIndex;
+	        for(var m=0;m<12;m++){
+	        	if(currentMonth == 11){
+	        		currentMonth = 0;
+	        	}else{
+	        		currentMonth++;
+	        	}
+		        $('.bubbleUlVert').append('<li></li>');
+		        $('.verticalLineUlMobile').append('<li class="verticalLineMobile"></li>');		        
+		        $('.dateDivVert').append('<li class="specificDateVert" style="color: rgb(245, 130, 158);">'+months[currentMonth]+'</li>');
+	        }	        
+        }
+        colorTimeline();
+        highlightTimelinePoint();
+        // unHightlightTimelinePoints();
+	});	
+	$('.login-link-and-dropdown-list a.login-link-text').on('click',function(){
+	  $('#login-dropdown-list').slideToggle();
+	});
+});
+var calculateYears = function(startDate) {
+    var now = new Date();
+    var past = new Date(startDate);
+    var nowYear = now.getFullYear();
+    var pastYear = past.getFullYear();
+    var years = nowYear - pastYear;
+    return years;
+};
+function monthDiff(d1, d2) {
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
+function colorTimeline(){
+	$('.specificDateVert,.verticalLineMobile,.bubbleUlVert li').on('mouseover',function(){
+	  var currentIndex = $(this).index();
+	  $('.timelineVertical ul li:nth-child('+(currentIndex+1)+')').addClass('active');
+	})
+	$('.specificDateVert,.verticalLineMobile,.bubbleUlVert li').on('mouseout',function(){
+	  var currentIndex = $(this).index();
+	  $('.timelineVertical ul li:nth-child('+(currentIndex+1)+')').removeClass('active');
+	})		
+}
+function highlightTimelinePoint(){
+	$('.specificDateVert,.verticalLineMobile,.bubbleUlVert li').on('click',function(){
+		var currentIndex = $(this).index();
+		$('.timelineVertical ul li:nth-child('+(currentIndex+1)+')').addClass('show');
+		$('.timelineVertical ul li:not(:nth-child('+(currentIndex+1)+'))').addClass('no-show');
+		$('#calendar').datepicker( "show" );		
+		console.log(currentIndex)
+		console.log(currentIndex%2 == 0)
+		if(currentIndex%2){
+			$('.timelineVertical').css('margin-left','-40%');
+			$('.ui-datepicker-calendar').attr('align','center')				
+		}else{
+			$('.timelineVertical').css('margin-left','40%');
+			$('.ui-datepicker-calendar').attr('align','center')					
+		}
+	});	
+	unHightlightTimelinePoints();
+}
+function unHightlightTimelinePoints(){
+	$('body').on('click',function(event){
+		if(!$(event.target).hasClass('show')){
+			$('.timeline-event').fadeOut();
+			$('.arrow_box').fadeOut();
+			revertToCenter();
+		}
+	});
+}
+function revertToCenter(){
+	$('.timelineVertical').css('margin-left','0');
+	$('.timelineVertical ul li').removeClass('no-show');
+	$('.timelineVertical ul li').removeClass('show');
+}
+// function addTimelineBubbles(){
+// 	$('.bubbleUlVert li').on('click',function(){
+// 		console.log('inserting')
+// 		$(this).after('<li class="timeline-event" style="height:10px;width:10px;margin-left:-3px;background:purple;position:absolute;"></li>')
+// 	});
+// }
---
+++ timeline-meteor.js
@@ -0,0 +1,76 @@
+var StartDate = new Mongo.Collection("startDate");
+var newEvent = new Mongo.Collection("newevent");
+if (Meteor.isClient) {
+	Template.modalButtons.events({
+		"click .choose-date": function (event) {
+			if( Meteor.user() ){
+				newEvent.insert({
+					type:event.target.id,
+					name: $('.event-name').val(),
+					description: $('.event-description').val(),
+					date: $('#date-picker').val(),
+					createdAt: new Date(),
+					user: Meteor.user()._id,
+					email: Meteor.user().emails[0].address
+				});
+			}else{
+			}
+		}
+	});
+	Template.body.events({
+		"blur #start-date": function (event) {
+			if( Meteor.user() && !StartDate.find({user:Meteor.user()._id}).fetch().length){
+				var date = event.target.value;
+				StartDate.insert({
+					date: date,
+					createdAt: new Date(), // current time
+					user: Meteor.user()._id,
+					email: Meteor.user().emails[0].address
+				});
+			}else{
+
+			}
+		},
+		"click .specificDateVert, click .verticalLineMobile, click .bubbleUlVert li": function (event) {
+			if( Meteor.user() ){
+				var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+				$('.timeline-event').remove();
+				$('.arrow_box').remove();
+				var currentIndex = $(event.target).index();
+				var chosenMonth = $('.leftDate .specificDateVert:nth-child('+(currentIndex+1)+')').text();
+				var currentYear = (new Date).getFullYear().toString();
+				var userEvents = newEvent.find({user:Meteor.user()._id}).fetch();
+				var count = 0;
+				$.each(userEvents,function(index,thisEvent){
+					var monthWord = months[Number(thisEvent.date.split('/')[0]-1)];
+					var year = thisEvent.date.split('/')[2];
+					if( (monthWord == chosenMonth && currentYear == year) || year.toString() == chosenMonth){
+						$('.bubbleUlVert li:nth-child('+(currentIndex+1)+')').after('<li class="timeline-event offset-'+count+'"></li>');
+						$('.timelineVertical').prepend('<div class="arrow_box offset-'+count+'"><p>'+thisEvent.description+'</p></div>');
+						count++;
+					}
+				});
+				for (var i=0;i<count;i++){
+					var heightSet = $('.arrow_box.offset-'+i).height();
+					// $('.arrow_box.offset-'+i).css('margin-top',(40+heightSet)+"px"); 
+					$('.timeline-event.offset-'+(i-1) ).css('margin-top',40+(heightSet)+"px");				
+				}
+				for (var it=0;it<count;it++){
+					var desiredOffset = $('.timeline-event.offset-'+it).offset();
+					console.log(desiredOffset)
+					// $('.arrow_box.offset-'+it).offset({top: desiredOffset.top-20,left: desiredOffset.left-($('.arrow_box.offset-'+it).width()+40)});				
+					$('.arrow_box.offset-'+it).offset({top: desiredOffset.top-20,left: desiredOffset.left + 40 });				
+				}
+			}
+		}	
+	});	
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    
+  });
+}
+
